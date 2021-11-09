8aadb47948f46a7f47dca7d616412f247bff6d2a
Matej <matejchalachan@gmail.com>
2021-11-09 22:18:29 +0100

    content of models added

--- src/models/PageGuide.php
+++ src/models/PageGuide.php
@@ -4,7 +4,44 @@ namespace matejch\pageGuide\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * This is the model class for table "page_guide".
+ *
+ * @property int $id
+ * @property string|null $url
+ * @property string|null $rules
+ */
 class PageGuide extends ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'page_guide';
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['url'], 'required'],
+            [['rules'], 'string'],
+            [['url'], 'string', 'max' => 1024],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'url' => 'Url',
+            'rules' => 'Rules',
+        ];
+    }
 }
