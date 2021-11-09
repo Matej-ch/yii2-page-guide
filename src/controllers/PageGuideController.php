9add659c1e23c7273fca5fd9ab49d9809a27151d
Matej <matejchalachan@gmail.com>
2021-11-09 22:09:21 +0100

    controller added

--- src/controllers/PageGuideController.php
+++ src/controllers/PageGuideController.php
@@ -2,9 +2,106 @@
 
 namespace matejch\pageGuide\controllers;
 
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\helpers\Json;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 class PageGuideController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' =>[
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['index','update','create','delete'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ]
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
 
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => PageGuide::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionCreate()
+    {
+        $model = new PageGuide();
+
+        $post = Yii::$app->request->post();
+        if(Yii::$app->request->isPost) {
+            $post['Guide']['url'] = parse_url($post['Guide']['url'],PHP_URL_PATH);
+
+            $post['Guide']['rules'] = array_filter($post['Guide']['rules'], static function ($val) {
+                return !empty($val['element']);
+            });
+
+            $post['Guide']['rules'] = Json::encode($post['Guide']['rules']);
+
+            PageGuide::deleteAll(['url' => $post['Guide']['url']]);
+
+            if ($model->load($post) && $model->save()) {
+                return $this->redirect(['index']);
+            }
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if(Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            $post['Guide']['rules'] = Json::encode($post['Guide']['rules']);
+
+            if ($model->load($post) && $model->save()) {
+                return $this->redirect(['index']);
+            }
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = PageGuide::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('Not found');
+    }
 }
