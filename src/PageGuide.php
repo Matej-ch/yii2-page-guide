fc81c57b23031e9f0843a0dfddebb9b6e539996e
Matej <matejchalachan@gmail.com>
2021-11-12 14:52:53 +0100

    reginstering translations

--- src/PageGuide.php
+++ src/PageGuide.php
@@ -2,6 +2,7 @@
 
 namespace matejch\pageGuide;
 
+use Yii;
 use yii\base\Module;
 
 class PageGuide extends Module
@@ -13,5 +14,27 @@ class PageGuide extends Module
     public function init()
     {
         parent::init();
+
+        \Yii::setAlias('@matejch/pageGuide', __DIR__);
+
+        $this->registerTranslations();
+    }
+
+    public function registerTranslations()
+    {
+        Yii::$app->i18n->translations['pageGuide'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => 'en',
+            'basePath' => '@matejch/pageGuide/messages',
+            'fileMap' => [
+                'modules/users/view' => 'view.php',
+                'modules/users/model' => 'model.php',
+            ],
+        ];
+    }
+
+    public static function t($category, $message, $params = [], $language = null)
+    {
+        return Yii::t('@matejch/pageGuide/' . $category, $message, $params, $language);
     }
 }
