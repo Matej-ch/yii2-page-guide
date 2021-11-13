243ad0cf46ce4175d70116b8fccbc6f93bb139e5
Matej <matejchalachan@gmail.com>
2021-11-13 22:18:20 +0100

    javasritp moved to file

--- src/views/page-guide/_form.php
+++ src/views/page-guide/_form.php
@@ -2,7 +2,6 @@
 
 use yii\helpers\{Html, Json};
 use yii\widgets\ActiveForm;
-use yii\web\View;
 
 /* @var $this yii\web\View */
 /* @var $model \matejch\pageGuide\models\PageGuide */
@@ -21,7 +20,9 @@ use yii\web\View;
                 <p><?= Yii::t('pageGuide/view','help_3') ?></p>
                 <div class="container-flex">
                     <input type="text" id="js-url-input" class="form-control">
-                    <?= Html::a(Yii::t('pageGuide/view','goto'),null,['class' => 'btn btn-primary','id' => 'js-goto-page'])?>
+                    <div class="my-2">
+                        <?= Html::a(Yii::t('pageGuide/view','goto'),null,['class' => 'btn btn-primary','id' => 'js-goto-page'])?>
+                    </div>
                 </div>
             </div>
 
@@ -51,79 +52,3 @@ use yii\web\View;
     </div>
 
 <?= $this->render('templates/_rule') ?>
-
-<?php
-$scriptGuideForm = <<< JS
-var click = 1;
-
-document.querySelector('body').addEventListener('click',function(e) {
-  if(e.target.id === 'js-goto-page') {
-      e.preventDefault();
-      if(document.getElementById('js-url-input') && document.getElementById('js-url-input').value !== '') {
-          const url = new URL(document.getElementById('js-url-input').value);
-          if(Array.from(url.searchParams).length) {
-              window.open(document.getElementById('js-url-input').value + '&guideCreator=1','_blank'); 
-          } else {
-              window.open(document.getElementById('js-url-input').value + '?guideCreator=1','_blank');
-          }
-      }
-  }
-});
-
-function addElement() {
-    let tmpl = document.getElementById('element-input-template').content.cloneNode(true);
-    tmpl.querySelector('.js-step').name = 'Guide[rules]['+click+'][step]';
-    tmpl.querySelector('.js-step').value = click + 1;
-    tmpl.querySelector('.js-element').name = 'Guide[rules]['+click+'][element]';
-    tmpl.querySelector('.js-intro').name = 'Guide[rules]['+click+'][intro]';
-    document.getElementById('js-el-container').appendChild(tmpl);
-    click++;
-}
-
-  /* events fired on the drop targets */
-document.addEventListener("dragover", function( event ) {
-  // prevent default to allow drop
-  event.preventDefault();
-}, false);
-
-document.addEventListener("dragenter", function( event ) {
-  // highlight potential drop target when the draggable element enters it
-  if (event.target.classList && event.target.classList.contains("js-dragzone")) {
-      event.target.style.background = "#e83b3b";
-  }
-}, false);
-
-document.addEventListener("dragleave", function( event ) {
-      // reset background of potential drop target when the draggable element leaves it
-  if (event.target.classList && event.target.classList.contains("js-dragzone")) {
-      event.target.style.background = "";
-  }
-  }, false);
-
-document.addEventListener("drop", function( event ) {
-  // prevent default action (open as link for some elements)
-  event.preventDefault();
-  // move dragged elem to the selected drop target
-  if (event.target.classList && event.target.classList.contains("js-dragzone")) {
-      event.target.style.background = "";
-      onDrop(event);
-      //dragged.parentNode.removeChild( dragged );
-      //event.target.appendChild( dragged );
-  }
-
-}, false);
-
-
-function onDrop(event) {
-  const id = event.dataTransfer.getData('id');
-  
-  //const draggableElement = document.getElementById(id);
-  const dropzone = event.target;
-  dropzone.parentElement.querySelector('.js-element').value = "#" + id;
-  //dropzone.appendChild(draggableElement);
-  addElement();
-  //event.dataTransfer.clearData();
-}
-
-JS;
-$this->registerJs($scriptGuideForm,View::POS_READY,'scriptGuideForm');
--- src/web/js/assist.js
+++ src/web/js/assist.js
@@ -35,5 +35,69 @@ document.addEventListener('DOMContentLoaded',() => {
         event.dataTransfer.setData('id', event.target.id);
         event.target.style.backgroundColor = '#8db3f2';
     }
+
+    var click = 1;
+
+    document.querySelector('body').addEventListener('click',function(e) {
+        if(e.target.id === 'js-goto-page') {
+            e.preventDefault();
+            if(document.getElementById('js-url-input') && document.getElementById('js-url-input').value !== '') {
+                const url = new URL(document.getElementById('js-url-input').value);
+                if(Array.from(url.searchParams).length) {
+                    window.open(document.getElementById('js-url-input').value + '&guideCreator=1','_blank');
+                } else {
+                    window.open(document.getElementById('js-url-input').value + '?guideCreator=1','_blank');
+                }
+            }
+        }
+    });
+
+    function addElement() {
+        let tmpl = document.getElementById('element-input-template').content.cloneNode(true);
+        tmpl.querySelector('.js-step').name = 'Guide[rules]['+click+'][step]';
+        tmpl.querySelector('.js-step').value = click + 1;
+        tmpl.querySelector('.js-element').name = 'Guide[rules]['+click+'][element]';
+        tmpl.querySelector('.js-intro').name = 'Guide[rules]['+click+'][intro]';
+        document.getElementById('js-el-container').appendChild(tmpl);
+        click++;
+    }
+
+    /* events fired on the drop targets */
+    document.addEventListener("dragover", function( event ) {
+        // prevent default to allow drop
+        event.preventDefault();
+    }, false);
+
+    document.addEventListener("dragenter", function( event ) {
+        // highlight potential drop target when the draggable element enters it
+        if (event.target.classList && event.target.classList.contains("js-dragzone")) {
+            event.target.style.background = "#e83b3b";
+        }
+    }, false);
+
+    document.addEventListener("dragleave", function( event ) {
+        // reset background of potential drop target when the draggable element leaves it
+        if (event.target.classList && event.target.classList.contains("js-dragzone")) {
+            event.target.style.background = "";
+        }
+    }, false);
+
+    document.addEventListener("drop", function( event ) {
+        // prevent default action (open as link for some elements)
+        event.preventDefault();
+        // move dragged elem to the selected drop target
+        if (event.target.classList && event.target.classList.contains("js-dragzone")) {
+            event.target.style.background = "";
+            onDrop(event);
+        }
+
+    }, false);
+
+    function onDrop(event) {
+        const id = event.dataTransfer.getData('id');
+        const dropzone = event.target;
+        dropzone.parentElement.querySelector('.js-element').value = "#" + id;
+        addElement();
+    }
 });
 
