d1a656f669e207234f8272cbc813fc1d56132a9d
Matej <matejchalachan@gmail.com>
2021-11-14 20:19:21 +0100

    adding styles for btn

--- src/widget/views/_assist.php
+++ src/widget/views/_assist.php
@@ -1,9 +1,12 @@
 <?php
 
+/** @var array $btnPositionCss */
+
 use yii\helpers\Html;
 
 ?>
 
-<?= Html::a('<i class="fas fa-question-circle text-2xl text-blue-500"></i>',
+<?=
+Html::a('<i class="fas fa-question-circle text-2xl text-blue-500"></i>',
     null,
-    ['class' => 'bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l cursor-pointer' ,'id' => 'js-intro-guide','style' => $btnPositionCss]) ?>
+    ['class' => 'bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded cursor-pointer' ,'id' => 'js-intro-guide','style' => $btnPositionCss]) ?>
